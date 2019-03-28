d05e3023ee8639a3c08d957346fcf19980a999cc
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-03-28 16:31:10 -0400

    movement logic

--- elm/src/Game.elm
+++ elm/src/Game.elm
@@ -2,18 +2,34 @@ module Game exposing (main)
 
 import Browser
 import Browser.Dom  exposing (getViewport)
-import Browser.Events exposing (onKeyDown, onKeyUp)
+import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp)
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (keyCode)
 import Json.Decode as Decode
+import String exposing (fromInt)
 import Svg exposing (svg, image)
 import Svg.Attributes exposing (xlinkHref, x, y)
 
-type alias Model = { playing: Bool }
+type alias Player =
+  {
+    x: Int,
+    y: Int,
+    vx: Int,
+    vy: Int,
+    sprite: String
+  }
+
+initPlayer = { x = 50, y = 200, vx = 0, vy = 0, sprite = "../images/sprite.png" }
+
+type alias Model = 
+  {
+    playing: Bool,
+    player: Player
+  }
 
 initModel : Model
-initModel =  { playing = True }
+initModel =  { playing = True, player = initPlayer }
 
 view : Model -> Html Msg
 view model =
@@ -30,35 +46,55 @@ view model =
         , style "width" "100%"
         ]
         [ image
-          [ xlinkHref "../images/sprite.png", x "50", y "200" ]
+          [ xlinkHref model.player.sprite, x (fromInt model.player.x), y (fromInt model.player.y) ]
           []
         ]
       else
         div [] []
     ]
 
-type Msg = MoveLeft Bool | MoveRight Bool | MoveUp Bool | MoveDown Bool | Noop
+type Direction = Up | Left | Down | Right
+
+type Msg = Move Direction Bool | Tick Float | Noop
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
+  let player = model.player in
   case msg of
-    MoveLeft True ->
-      ( model, Cmd.none)
-    MoveLeft False ->
-      ( model, Cmd.none)
-    MoveRight _ ->
-      (model, Cmd.none)
-    MoveUp _ ->
-      (model, Cmd.none)
-    MoveDown _ ->
-      (model, Cmd.none)
+    Move dir on ->
+      case dir of
+        Up ->
+          if on then
+            ( { model | player = { player | vy = -2 } }, Cmd.none)
+          else
+            ( { model | player = { player | vy = 0 } }, Cmd.none)
+        Left ->
+          if on then
+            ( { model | player = { player | vx = -2 } }, Cmd.none)
+          else
+            ( { model | player = { player | vx = 0 } }, Cmd.none)
+        Down ->
+          if on then
+            ( { model | player = { player | vy = 2 } }, Cmd.none)
+          else
+            ( { model | player = { player | vy = 0 } }, Cmd.none)
+        Right ->
+          if on then
+            ( { model | player = { player | vx = 2 } }, Cmd.none)
+          else
+            ( { model | player = { player | vx = 0 } }, Cmd.none)
+    Tick _ ->
+      let newX = player.x + player.vx in
+      let newY = player.y + player.vy in
+      -- TODO: animate sprite movement (change image)
+      ( { model | player = { player | x = newX, y =  newY} }, Cmd.none)
     Noop ->
       (model, Cmd.none)
 
 type Key = W | A | S | D | Space | Other
 
-keyOfKeyCode : Int -> Key
-keyOfKeyCode keycode =
+key : Int -> Key
+key keycode =
   case keycode of
     87 -> W
     65 -> A
@@ -67,26 +103,29 @@ keyOfKeyCode keycode =
     32 -> Space
     _ -> Other
 
-key : Bool -> Int -> Msg
-key on keycode =
-  case keyOfKeyCode keycode of
+msgFromKeyCode : Bool -> Int -> Msg
+msgFromKeyCode on keycode =
+  case key keycode of
     W ->
-      MoveUp on
+      Move Up on
     A ->
-      MoveLeft on
+      Move Left on
     S ->
-      MoveDown on
+      Move Down on
     D ->
-      MoveRight on
+      Move Right on
     _ ->
       Noop
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch
-        [ onKeyUp (Decode.map (key False) (keyCode))
-        , onKeyDown (Decode.map (key True) (keyCode))
-        ]
+  Sub.batch
+    [ if model.playing then
+        onAnimationFrameDelta Tick
+      else
+       Sub.none
+    , onKeyUp (Decode.map (msgFromKeyCode False) keyCode)
+    , onKeyDown (Decode.map (msgFromKeyCode True) keyCode) ]
 
 main : Program () Model Msg
 main =

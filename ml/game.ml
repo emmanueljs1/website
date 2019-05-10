4a2f721ebb28411832ae792d26076c8225364570
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-05-09 23:51:58 -0400

    update ml code to move sprite

--- ml/game.ml
+++ ml/game.ml
@@ -3,6 +3,7 @@ open Tea.Html
 module Svg = Tea.Svg
 module Sub = Tea.Sub
 module Cmd = Tea.Cmd
+module AnimationFrame = Tea.AnimationFrame
 
 let lower_x_bound = 0
 
@@ -42,14 +43,56 @@ type direction =
   | Right
 
 type msg =
-  | Move of direction * bool
-  | Tick of float
+  | Tick of AnimationFrame.t
+  | KeyDown of Keyboard.key_event
+  | KeyUp of Keyboard.key_event
   | Noop
   [@@bs.deriving {accessors}]
 
+let dir_of_key (key: Keyboard.key) : direction option =
+  match key with
+  | W ->
+    Some Up
+  | A ->
+    Some Left
+  | S ->
+    Some Down
+  | D ->
+    Some Right
+  | _ -> None
+
 let update (model: model) (msg: msg) : model * msg Cmd.t =
-  match msg with
-  | _ -> model, Cmd.none
+  let player = model.player in
+  let model' =
+    match msg with
+    | KeyDown event ->
+      begin match event.key_code |> Keyboard.key_of_keycode |> dir_of_key with
+      | Some dir ->
+        begin match dir with
+        | Up -> { model with player = { player with vy = -3 } }
+        | Left -> { model with player = { player with vx = -3 } }
+        | Down -> { model with player = { player with vy = 3 } }
+        | Right -> { model with player = { player with vx = 3 } }
+        end
+      | None -> model
+      end
+    | KeyUp event ->
+      begin match event.key_code |> Keyboard.key_of_keycode |> dir_of_key with
+      | Some dir ->
+        begin match dir with
+        | Up | Down -> { model with player = { player with vy = 0 } }
+        | Left | Right -> { model with player = { player with vx = 0 } }
+        end
+      | None -> model
+      end
+    | Tick _ ->
+      let newX = player.x + player.vx in
+      let newY = player.y + player.vy in
+      let newPlayer = { player with x = newX; y =  newY } in
+      { model with player = newPlayer }
+    | _ -> model
+  in
+  model', Cmd.none
 
 let view (model: model) : msg Vdom.t =
   div
@@ -78,11 +121,15 @@ let view (model: model) : msg Vdom.t =
     ]
 
 let subscriptions (model: model) : msg Sub.t =
-  failwith "unimplemented"
+  [ Keyboard.downs keyDown
+  ; Keyboard.ups keyUp
+  ; if model.playing then AnimationFrame.every tick else Sub.none
+  ] |> Sub.batch
 
-let main : (unit, model, msg) standardProgram =
-  { init = (fun () -> (init (), Cmd.none))
-  ; update = update
-  ; view = view
-  ; subscriptions = subscriptions
-  }
+let main =
+  standardProgram
+    { init = (fun () -> (init (), Cmd.none))
+    ; update = update
+    ; view = view
+    ; subscriptions = subscriptions
+    }
